3b6cd9aabaca7dc9b22b01701310dfc253530b73
OscarLight <oscarli2716@gmail.com>
2017-04-21 16:58:47 -0400

    add windows support

--- statistics.py
+++ statistics.py
@@ -31,3 +31,15 @@ class IonStatistics:
                                   self.velocities[job][i][0], self.velocities[job][i][1],
                                   self.velocities[job][i][2]))
         outputFile.close()
+
+    def to_csv2(self, filename = 'tmp.csv'):
+        outputFile = open(filename, 'w')
+        outputFile.write('id,x,y,z,u,v,w\n')
+    
+        for i in range(len(self.ids)):
+            outputFile.write('%d,%2.3f,%2.3f,%2.3f,%2.3f,%2.3f,%2.3f\n' %
+                                 (self.ids[i], self.positions[i][0],
+                                  self.positions[i][1], self.positions[i][2],
+                                  self.velocities[i][0], self.velocities[i][1],
+                                  self.velocities[i][2]))
+        outputFile.close()
